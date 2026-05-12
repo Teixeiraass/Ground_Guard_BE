46cdbd1ae73a0769ed53999926435710d5d14ccc
Teixeiraass <guist2017@hotmail.com>
2026-05-11 21:07:58 -0300

    fix: declaracao nao usada removida

--- api/device_test.go
+++ api/device_test.go
@@ -1,29 +1,29 @@
 package api
 
-import (
-	"database/sql"
-	"testing"
+// import (
+// 	"database/sql"
+// 	"testing"
 
-	db "github.com/Teixeiraass/ground_guard_be/db/sqlc"
-	"github.com/Teixeiraass/ground_guard_be/util"
-)
+// 	db "github.com/Teixeiraass/ground_guard_be/db/sqlc"
+// 	"github.com/Teixeiraass/ground_guard_be/util"
+// )
 
-func TestGetDeviceAPI(t *testing.T) {
-	user, _ := randomUser(t)
-	device := randomDevice(user.ID)
-}
+// func TestGetDeviceAPI(t *testing.T) {
+// 	user, _ := randomUser(t)
+// 	device := randomDevice(user.ID)
+// }
 
-func randomDevice(userID int64) db.Device {
-	return db.Device{
-		ID: util.RandomInt(1, 1000),
-		Uuid: util.RandomUuid(),
-		DeviceUid: util.RandomString(10),
-		Name: util.RandomString(6),
-		FirmwareVersion: util.RandomString(8),
-		Status: util.RandomStatus(),
-		UserID: sql.NullInt64{
-			Int64: userID,
-			Valid: true,
-		},
-	}
-}
+// func randomDevice(userID int64) db.Device {
+// 	return db.Device{
+// 		ID: util.RandomInt(1, 1000),
+// 		Uuid: util.RandomUuid(),
+// 		DeviceUid: util.RandomString(10),
+// 		Name: util.RandomString(6),
+// 		FirmwareVersion: util.RandomString(8),
+// 		Status: util.RandomStatus(),
+// 		UserID: sql.NullInt64{
+// 			Int64: userID,
+// 			Valid: true,
+// 		},
+// 	}
+// }
